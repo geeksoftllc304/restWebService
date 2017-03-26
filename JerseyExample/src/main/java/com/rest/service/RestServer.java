c4d0b8fb52108dd49d4cbcb8597ca7e1f12c0828
KRISH\gopid <gopidaggupati@gmail.com>
2017-03-26 13:28:31 -0400

    Restful_Jersey created by gopi

---
+++ JerseyExample/src/main/java/com/rest/service/RestServer.java
@@ -0,0 +1,44 @@
+package com.rest.service;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.restservice.model.Student;
+
+@Path("/student/data")
+public class RestServer {
+	
+	@GET
+	@Path("/get")
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	public Student getStudentRecord()
+	{
+		
+		Student student =new Student();
+		student.setFirstName("John");
+		student.setLastName("Kerry");
+		student.setSchool("Standford");
+		student.setStandard("ONE");
+		student.setRollNumber(222);
+		
+		return student;
+		
+	}
+	
+	@POST
+	@Path("/post")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response postStudentRecord( Student student)
+	{
+		
+		String result="Record Created "+ student;
+		return Response.status(201).entity(result).build();
+		
+	}
+
+}
